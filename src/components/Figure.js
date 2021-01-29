f5d0388b5202187af90684aae2e1fd741325e951
DiegoMutre <tysonmutre10@gmail.com>
2021-01-28 19:16:36 -0500

    feat: Create Figure component (not finished)

---
+++ src/components/Figure.js
@@ -0,0 +1,10 @@
+const Figure = () => {
+    return (
+        <svg height="250" width="200" className="figure-container">
+            {/* Rod */}
+            <line x1="60" y1="20" x2="140" y2="20" />
+        </svg>
+    );
+};
+
+export default Figure;
