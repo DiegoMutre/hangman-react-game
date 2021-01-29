718c63c476fc05787f24de408a0569a393e9a848
DiegoMutre <tysonmutre10@gmail.com>
2021-01-28 19:32:59 -0500

    feat: Create the states.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 import Figure from "./components/Figure";
 import Header from "./components/Header";
 
 function App() {
+    // States
+    const [isPlayable, setIsPlayable] = useState(true);
+    const [correctLetters, setCorrectLetters] = useState([]);
+    const [wrongLetters, setWrongLetters] = useState([]);
+    const [showNotification, setShowNotification] = useState(false);
+
     return (
         <>
             <Header />
