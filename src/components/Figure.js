88f2c101577a1402f8b35dc6450e717bd7d97144
DiegoMutre <tysonmutre10@gmail.com>
2021-01-30 21:47:50 -0500

    feat: Add a animation to each figure

--- src/components/Figure.js
+++ src/components/Figure.js
@@ -13,23 +13,70 @@ const Figure = ({ wrongLetters }) => {
 
             {/* Head */}
 
-            {errors > 0 && <circle cx="140" cy="70" r="20" />}
+            {errors > 0 && (
+                <circle
+                    cx="140"
+                    cy="70"
+                    r="20"
+                    className="animate__animated animate__flash"
+                />
+            )}
 
             {/* Body */}
 
-            {errors > 1 && <line x1="140" y1="90" x2="140" y2="150" />}
+            {errors > 1 && (
+                <line
+                    x1="140"
+                    y1="90"
+                    x2="140"
+                    y2="150"
+                    className="animate__animated animate__flash"
+                />
+            )}
 
             {/* Arms */}
 
-            {errors > 2 && <line x1="140" y1="120" x2="120" y2="100" />}
+            {errors > 2 && (
+                <line
+                    x1="140"
+                    y1="120"
+                    x2="120"
+                    y2="100"
+                    className="animate__animated animate__flash"
+                />
+            )}
 
-            {errors > 3 && <line x1="140" y1="120" x2="160" y2="100" />}
+            {errors > 3 && (
+                <line
+                    x1="140"
+                    y1="120"
+                    x2="160"
+                    y2="100"
+                    className="animate__animated animate__flash"
+                />
+            )}
 
             {/* Legs */}
 
-            {errors > 4 && <line x1="140" y1="150" x2="120" y2="180" />}
+            {errors > 4 && (
+                <line
+                    x1="140"
+                    y1="150"
+                    x2="120"
+                    y2="180"
+                    className="animate__animated animate__flash"
+                />
+            )}
 
-            {errors > 5 && <line x1="140" y1="150" x2="160" y2="180" />}
+            {errors > 5 && (
+                <line
+                    x1="140"
+                    y1="150"
+                    x2="160"
+                    y2="180"
+                    className="animate__animated animate__flash"
+                />
+            )}
         </svg>
     );
 };
