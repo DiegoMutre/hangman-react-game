203b3d0ca1d88bbfbedcf59c010453c06f691bdd
DiegoMutre <tysonmutre10@gmail.com>
2021-02-11 06:36:49 -0500

    refactor: Refactor Word component.

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,15 +1,15 @@
 import PropTypes from "prop-types";
 
-const Word = ({ selectedWord, correctLetters }) => (
-    <div className="word">
-        {selectedWord.split("").map((letter, i) => (
-            <span className="letter" key={i}>
-                {/* Doesn't display the correct word until values are inserted */}
-                {correctLetters.includes(letter) ? letter : ""}
-            </span>
-        ))}
-    </div>
-);
+const Word = ({ selectedWord = "", correctLetters = [] }) => {
+    // Displays each letter of the selected word if the correct letters includes it
+    const completedWord = [...selectedWord].map((letter, i) => (
+        <span key={i} className="letter">
+            {correctLetters.includes(letter) && letter}
+        </span>
+    ));
+
+    return <div className="word">{completedWord}</div>;
+};
 
 Word.propTypes = {
     selectedWord: PropTypes.string.isRequired,
