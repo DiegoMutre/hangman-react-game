be6c6a813486d2b602a81f2fe4daa4363feeec9c
DiegoMutre <tysonmutre10@gmail.com>
2021-02-11 06:13:32 -0500

    refactor: Comment everything that is refactored in the App component, this part is unable and doesn't work.

--- src/App.js
+++ src/App.js
@@ -8,77 +8,75 @@ import Word from "./components/Word";
 import WrongLetters from "./components/WrongLetters";
 import { showNotification as show } from "./helpers/helper";
 
-const words = ["application", "programming", "interface", "wizard"];
-let selectedWord = words[Math.floor(Math.random() * words.length)];
+// const words = ["application", "programming", "interface", "wizard"];
+// let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
     // States
-    const [isPlayable, setIsPlayable] = useState(true);
-    const [correctLetters, setCorrectLetters] = useState([]);
-    const [wrongLetters, setWrongLetters] = useState([]);
-    const [showNotification, setShowNotification] = useState(false);
+    // const [isPlayable, setIsPlayable] = useState(true);
+    // const [correctLetters, setCorrectLetters] = useState([]);
+    // const [wrongLetters, setWrongLetters] = useState([]);
+    // const [showNotification, setShowNotification] = useState(false);
 
     // Add keydown event to global window
     useEffect(() => {
         const handleKeyDown = ({ key, keyCode }) => {
-            if (isPlayable && keyCode >= 65 && keyCode <= 90) {
-                const letter = key.toLowerCase();
-                if (selectedWord.includes(letter)) {
-                    !correctLetters.includes(letter)
-                        ? setCorrectLetters(currentLetters => [
-                              ...currentLetters,
-                              letter,
-                          ])
-                        : show(setShowNotification);
-                } else {
-                    !wrongLetters.includes(letter)
-                        ? setWrongLetters(currentLetters => [
-                              ...currentLetters,
-                              letter,
-                          ])
-                        : show(setShowNotification);
-                }
-            }
+            // if (isPlayable && keyCode >= 65 && keyCode <= 90) {
+            //     const letter = key.toLowerCase();
+            //     if (selectedWord.includes(letter)) {
+            //         !correctLetters.includes(letter)
+            //             ? setCorrectLetters(currentLetters => [
+            //                   ...currentLetters,
+            //                   letter,
+            //               ])
+            //             : show(setShowNotification);
+            //     } else {
+            //         !wrongLetters.includes(letter)
+            //             ? setWrongLetters(currentLetters => [
+            //                   ...currentLetters,
+            //                   letter,
+            //               ])
+            //             : show(setShowNotification);
+            //     }
+            // }
         };
 
         window.addEventListener("keydown", handleKeyDown);
 
         // Remove the previous event in the next rendering
         return _ => window.removeEventListener("keydown", handleKeyDown);
-    }, [correctLetters, wrongLetters, isPlayable]);
+    }, []);
 
     const playAgain = _ => {
-        setIsPlayable(true);
-
-        // Empty arrays
-        setCorrectLetters([]);
-        setWrongLetters([]);
-
-        // Get random word
-        const random = Math.floor(Math.random() * words.length);
-        selectedWord = words[random];
+        // setIsPlayable(true);
+        // // Empty arrays
+        // setCorrectLetters([]);
+        // setWrongLetters([]);
+        // // Get random word
+        // const random = Math.floor(Math.random() * words.length);
+        // selectedWord = words[random];
     };
 
     return (
         <>
             <Header />
             <div className="game-container">
-                <Figure wrongLetters={wrongLetters} />
-                <WrongLetters wrongLetters={wrongLetters} />
+                <Figure />
+                <WrongLetters />
                 <Word
-                    correctLetters={correctLetters}
-                    selectedWord={selectedWord}
+                // correctLetters={correctLetters}
+                // selectedWord={selectedWord}
                 />
             </div>
 
             <Popup
-                correctLetters={correctLetters}
-                wrongLetters={wrongLetters}
-                selectedWord={selectedWord}
-                setIsPlayable={setIsPlayable}
-                playAgain={playAgain}
+            // correctLetters={correctLetters}
+            // wrongLetters={wrongLetters}
+            // selectedWord={selectedWord}
+            // setIsPlayable={setIsPlayable}
+            // playAgain={playAgain}
             />
-            <Notification showNotification={showNotification} />
+            <Notification />
         </>
     );
 }
